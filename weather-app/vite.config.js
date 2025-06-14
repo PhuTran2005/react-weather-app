30613b0cfcb7a6b5f600fb5ff63b04110e76c1b3
tranv <tranvanphu1882005z@gmail.com>
2025-06-14 21:03:38 +0700

    fix deploy

--- weather-app/vite.config.js
+++ weather-app/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/weather-app/", // Đường dẫn khi deploy lên sub-path (bắt buộc nếu không deploy ở root)
   plugins: [react()],
   build: {
     outDir: "dist",
